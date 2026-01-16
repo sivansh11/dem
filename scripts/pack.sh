16672c5a99b7030ebee57116f875f117d7480aa3
sivansh11 <sivanshgupta2002@gmail.com>
2026-01-17 01:37:17 +0530

    silent pack

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -2,5 +2,5 @@
 
 mkdir -p rootfs
 cd rootfs
-find . -print0 | cpio --null -ov --format=newc -R 0:0 > ../rootfs.cpio
+find . -print0 | cpio --null -ov --format=newc -R 0:0 > ../rootfs.cpio 2>/dev/null
 cd ../
